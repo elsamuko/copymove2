108df0ac408919c28d2518dcd24e3dc5449c61d3
Samuel Albrecht <elsamuko@gmail.com>
2015-06-28 19:49:35 +0200

    add: astyle formatter script

---
+++ scripts/format.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+MAIN_DIR="$SCRIPT_DIR/.."
+
+function format_code {
+    astyle \
+        --style=java \
+        --indent=spaces \
+        --indent-classes \
+        --indent-switches \
+        --indent-col1-comments \
+        --indent-namespaces \
+        --break-blocks \
+        --pad-oper \
+        --pad-paren-in \
+        --unpad-paren \
+        --add-brackets \
+        --align-pointer=type \
+        -r "$1"
+}
+
+format_code "$MAIN_DIR/src/*.cpp"
+format_code "$MAIN_DIR/src/*.hpp"
