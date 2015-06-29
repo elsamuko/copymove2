49ebb8a719fbcc653236b5ef3eb85227cb9d59c4
Samuel Albrecht <elsamuko@gmail.com>
2015-06-29 22:03:38 +0200

    chg: extended SorterParams

--- src/sorterparams.hpp
+++ src/sorterparams.hpp
@@ -1,11 +1,35 @@
 #pragma once
 
 #include <cstdlib>
+#include <string>
 
 class SorterParams {
     public:
         SorterParams();
 
-    public:
+        bool valid() const {
+            return mValidParams;
+        }
+        void setValid( const bool valid ) {
+            mValidParams = valid;
+        }
+
+        size_t minimalHits() const {
+            return mMinimalHits;
+        }
+        void setMinimalHits( const size_t hits ) {
+            mMinimalHits = hits;
+        }
+
+        std::string filename() const {
+            return mFilename;
+        }
+        void setFilename( const std::string& filename ) {
+            mFilename = filename;
+        }
+
+    private:
+        bool mValidParams;
         size_t mMinimalHits;
+        std::string mFilename;
 };
