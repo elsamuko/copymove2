5a93724ea25f968e84d398e2f2772804a145fd0b
Samuel Albrecht <elsamuko@gmail.com>
2015-07-05 18:52:31 +0200

    add: about dialog

---
+++ src/ui/about.cpp
@@ -0,0 +1,78 @@
+#include <QDesktopServices>
+#include <QMessageBox>
+#include <QDebug>
+
+#include "ui_about.h"
+#include "about.hpp"
+#include "log/log.hpp"
+#include "log/datetime.hpp"
+
+About::About( QWidget* parent ) :
+    QDialog( parent ),
+    ui( new Ui::About ) {
+
+    ui->setupUi( this );
+
+    /* no resize */
+    this->layout()->setSizeConstraint( QLayout::SetFixedSize );
+
+    /* version info */
+    QString message = tr( "Version: " )
+                      + QString::number( COPYMOVE_VERSION_MAJOR ) + "."
+                      + QString::number( COPYMOVE_VERSION_MINOR ) + "."
+                      + QString::number( COPYMOVE_VERSION_RELEASE ) + "\n";
+    message += QString( __DATE__ ) + " " + QString( __TIME__ );
+    ui->labelVersion->setText( message );
+
+    /* copyright information */
+    ui->labelCopyright->setText( "\u00A9 " + QString::number( datetime::compilationYear() )
+                                 + "\nFD Imaging UG (haftungsbeschr\u00E4nkt)" );
+
+    /* logo */
+    QPixmap pixmap( ":/icons/icons/logo.png" );
+    ui->labelLogo->setPixmap( pixmap.scaledToWidth( ui->labelLogo->width(), Qt::SmoothTransformation ) );
+
+    /* license info */
+    int i = 0;
+    QString dir = ":/licenses/licenses/";
+    ui->boxLicense->insertItem( i++, "Fontconfig",  QVariant( dir + "fontconfig.txt" ) );
+    ui->boxLicense->insertItem( i++, "ImageMagick", QVariant( dir + "imagemagick.txt" ) );
+    ui->boxLicense->insertItem( i++, "lcms2",       QVariant( dir + "lcms2.txt" ) );
+    ui->boxLicense->insertItem( i++, "libbz2",      QVariant( dir + "libbz2.txt" ) );
+    ui->boxLicense->insertItem( i++, "libfreetype", QVariant( dir + "libfreetype.txt" ) );
+    ui->boxLicense->insertItem( i++, "libiconv",    QVariant( dir + "libiconv.txt" ) );
+    ui->boxLicense->insertItem( i++, "libjpeg",     QVariant( dir + "libjpeg.txt" ) );
+    ui->boxLicense->insertItem( i++, "libltdl",     QVariant( dir + "libltdl.txt" ) );
+    ui->boxLicense->insertItem( i++, "liblzma",     QVariant( dir + "liblzma.txt" ) );
+    ui->boxLicense->insertItem( i++, "libopenjpeg", QVariant( dir + "libopenjpeg.txt" ) );
+    ui->boxLicense->insertItem( i++, "libpng",      QVariant( dir + "libpng.txt" ) );
+    ui->boxLicense->insertItem( i++, "libtiff",     QVariant( dir + "libtiff.txt" ) );
+    ui->boxLicense->insertItem( i++, "libxml2",     QVariant( dir + "libxml2.txt" ) );
+    ui->boxLicense->insertItem( i++, "libz",        QVariant( dir + "libz.txt" ) );
+    ui->boxLicense->insertItem( i++, "Qt",          QVariant( dir + "qt.txt" ) );
+}
+
+About::~About() {
+    delete ui;
+}
+
+void About::on_boxLicense_currentIndexChanged( int index ) {
+    QString filename = ui->boxLicense->itemData( index ).toString();
+
+    QFile file( filename );
+
+    if( file.open( QFile::ReadOnly | QFile::Text ) ) {
+        QTextStream stream( &file );
+        ui->browserLicense->setPlainText( stream.readAll() );
+    } else {
+        qWarning() << "Could not open " + filename;
+    }
+}
+
+void About::on_buttonWebsite_clicked() {
+    QDesktopServices::openUrl( QUrl( "http://www.fd-imaging.com" ) );
+}
+
+void About::on_buttonFeedback_clicked() {
+    QDesktopServices::openUrl( QUrl( "http://fd-imaging.com/apps-2/copymove/feedback/" ) );
+}
---
+++ src/ui/about.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <QDialog>
+
+namespace Ui {
+    class About;
+}
+
+//! \brief QDialog class to show the about and license texts
+class About : public QDialog {
+        Q_OBJECT
+
+    public:
+        explicit About( QWidget* parent = 0 );
+        ~About();
+
+    private slots:
+        void on_boxLicense_currentIndexChanged( int index );
+        void on_buttonWebsite_clicked();
+        void on_buttonFeedback_clicked();
+
+    private:
+        Ui::About* ui;
+};
