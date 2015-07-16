5fb5330e609dd3a56edbf6baf3c8c364176b27c0
Samuel Albrecht <elsamuko@gmail.com>
2015-07-16 23:12:37 +0200

    add: LOG_DEBUG

--- src/log/log.hpp
+++ src/log/log.hpp
@@ -2,14 +2,16 @@
 
 #include <string>
 
+#define LEVEL_DEBUG   " Debug  "
 #define LEVEL_INFO    " Info   "
 #define LEVEL_WARNING " Warning"
 #define LEVEL_ERROR   " Error  "
 
-#define LOG(A)         logging::writeLog( LEVEL_INFO, __FILE__, __LINE__, __FUNCTION__, A )
-#define LOG_INFO(A)    logging::writeLog( LEVEL_INFO, __FILE__, __LINE__, __FUNCTION__, A )
+#define LOG(A)         logging::writeLog( LEVEL_INFO,    __FILE__, __LINE__, __FUNCTION__, A )
+#define LOG_DEBUG(A)   logging::writeLog( LEVEL_DEBUG,   __FILE__, __LINE__, __FUNCTION__, A )
+#define LOG_INFO(A)    logging::writeLog( LEVEL_INFO,    __FILE__, __LINE__, __FUNCTION__, A )
 #define LOG_WARNING(A) logging::writeLog( LEVEL_WARNING, __FILE__, __LINE__, __FUNCTION__, A )
-#define LOG_ERROR(A)   logging::writeLog( LEVEL_ERROR, __FILE__, __LINE__, __FUNCTION__, A )
+#define LOG_ERROR(A)   logging::writeLog( LEVEL_ERROR,   __FILE__, __LINE__, __FUNCTION__, A )
 
 #define CHECK_QT_CONNECT( A ) if( !A ) { LOG_WARNING( "QObject::connect() failed" ); }
 
